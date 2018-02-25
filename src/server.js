0c928904c6356174a16b642b3cd886e810e1b732
Mats Nord <nord.mats@gmail.com>
2018-02-25 23:13:10 +0100

    Sopport for cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,36 @@
 const express = require('express');
+var cors = require('cors');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
+var logger = require('morgan');
+var fs = require('fs');
 require('babel-register');
 
 const bowlingControler = require('./controllers/bowling');
 
 const app = express();
+app.options('*', cors());
+app.use(cors());
+
+app.use(logger('common', {stream: fs.createWriteStream('./access.log', {flags: 'a'})}));
+app.use(logger('dev'));
+
+// Set basic headers to allow access from othter enviroment (port)
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST', 'OPTIONS');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+});
+
+app.use('/ng5', (req, res) => res.json([{ng5:"ok"}]));
 
 app.post('/api/play', jsonParser, bowlingControler );
 
@@ -14,6 +39,8 @@ app.get('/health', (req, res) => res.sendStatus(200));
 
 //So what is this...
 app.get('/', (req, res) => res.send("The bowling calculator service"));
+app.post('/', (req, res) => null);
+
 
 
 app.listen(3000, () => console.log('Bowling service listening on 3000!'))
